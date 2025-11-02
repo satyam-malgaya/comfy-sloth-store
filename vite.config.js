2380a46c808224558bb292b7e9ec460cfc6fe1c3
satyam-malgaya <satyam.malgaya@gmail.com>
2025-11-02 11:02:01 +0530

    updaing

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,14 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss(), npm],
-    test: {
-        environment: 'jsdom',
-        setupFiles: './src/setupTests.js',
-        globals: true,
-    },
+    plugins: [react(), tailwindcss()],
+    build: {
+        outDir: 'dist',
+        sourcemap: false
+    }
+    // test: {
+    //     environment: 'jsdom',
+    //     setupFiles: './src/setupTests.js',
+    //     globals: true,
+    // },
 })
