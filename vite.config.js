85257cefff1294b3620ea11f8471b334fdcdbd0d
satyam-malgaya <satyam.malgaya@gmail.com>
2025-11-02 12:15:52 +0530

    fixing eror

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     build: {
@@ -12,5 +12,5 @@ export default defineConfig({
         environment: 'jsdom',
         setupFiles: './src/setupTests.js',
         globals: true,
-    },
+    }
 })
